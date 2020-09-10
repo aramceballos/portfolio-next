dc78b1052917731c3df9ce19461b08d6aa7f82a0
Aram Ceballos <aramgonzalez12@hotmail.com>
2020-09-09 19:17:55 -0500

    Meta tags added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,22 +5,60 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head />
+
         <meta
           name='description'
           content='This is the portfolio of a Full-Stack JavaScript Developer which you can find their projects, certificates and info about him'
         />
-        <meta
-          name='keywords'
-          content='HTML, CSS, JS, JAVASCRIPT, FULL-STACK, FRONTEND, BACKEND, NODE.JS, REACT, REDUX, JEST, WEB, WEB DEVELOPER, REACT.JS, GRAPHQL'
-        />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='robots' content='index, follow' />
         <meta name='author' content='Aram Ceballos' />
+        <meta name='copyright' content='Aram Ceballos' />
+        <meta name='application-name' content='Aram Ceballos' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+        <meta name='apple-mobile-web-app-title' content='Aram Ceballos' />
+        <meta name='format-detection' content='telephone=no' />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='msapplication-TileColor' content='#1d1d1d' />
+        <meta name='msapplication-tap-highlight' content='no' />
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+        />
         <meta name='theme-color' content='#1d1d1d' />
+
+        <link rel='apple-touch-icon' sizes='180x180' href='/icon.png' />
         <link rel='manifest' href='/manifest.json' />
         <link rel='icon' href='/favicon.ico' />
+        <link rel='shortcut icon' href='/favicon.ico' />
         <link
           href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
           rel='stylesheet'></link>
+
+        <meta name='twitter:card' content='summary' />
+        <meta name='twitter:url' content='https://aramceballos.com' />
+        <meta name='twitter:title' content='Aram Ceballos' />
+        <meta
+          name='twitter:description'
+          content='This is the portfolio of a Full-Stack JavaScript Developer which you can find their projects, certificates and info about him'
+        />
+        <meta
+          name='twitter:image'
+          content='https://aramceballos.com/static/icons/android-chrome-192x192.png'
+        />
+        <meta name='twitter:creator' content='@aramgonzalez12' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Aram Ceballos' />
+        <meta
+          property='og:description'
+          content='This is the portfolio of a Full-Stack JavaScript Developer which you can find their projects, certificates and info about him'
+        />
+        <meta property='og:site_name' content='Aram Ceballos' />
+        <meta property='og:url' content='https://aramceballos.com' />
+        <meta
+          property='og:image'
+          content='https://aramceballos.com/static/icons/apple-touch-icon.png'
+        />
         <body>
           <Main />
           <NextScript />
